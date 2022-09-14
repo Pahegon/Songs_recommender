1351c218f111c4a9050e967e838d63bc5f0a3e66
Pahegon <hegondata@gmail.com>
2022-09-14 12:10:05 +0200

    3

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-client_id = "f85f3518e7a54397aef641454d8c78c9"
-client_secret= "71623af2cccd490ea273603de2ca5ef6"
+client_id = "98dc7c7d465a4e7fbe9af18fca43455a"
+client_secret= "c906d5a69e6a45dcb6019c3b04fc2cce"
